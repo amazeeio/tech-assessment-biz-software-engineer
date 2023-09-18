c26b7400c6a3e36015f432150914e4a54c89344a
Ognjen1994 <ognjen.risti@gmail.com>
2023-09-19 01:59:55 +0200

    Unit tests - add unit tests for books service and resolver

--- api/src/books/books.resolver.spec.ts
+++ api/src/books/books.resolver.spec.ts
@@ -1,15 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksResolver } from './books.resolver';
+import { BooksService } from './books.service';
 
 describe('BooksResolver', () => {
   let resolver: BooksResolver;
+  let booksService: BooksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BooksResolver],
+      providers: [BooksResolver, BooksService],
     }).compile();
 
     resolver = module.get<BooksResolver>(BooksResolver);
+    booksService = module.get<BooksService>(BooksService);
   });
 
   it('should be defined', () => {
@@ -37,6 +40,53 @@ describe('BooksResolver', () => {
         expect(book).toHaveProperty('author');
         expect(typeof book.author).toBe('string');
       });
+    });
+  });
+
+  describe('createBook', () => {
+    it('should create a new book', () => {
+      const mockCreateBook = jest.spyOn(booksService, 'createBook');
+
+      const title = 'New Book Title';
+      const author = 'New Book Author';
+
+      resolver.createBook(title, author);
+
+      expect(mockCreateBook).toHaveBeenCalledWith({
+        id: expect.any(String),
+        title,
+        author,
+      });
+    });
+  });
+
+  describe('updateBook', () => {
+    it('should update an existing book', () => {
+      const mockUpdateBook = jest.spyOn(booksService, 'updateBook');
+
+      const idToUpdate = '1';
+      const newTitle = 'Updated Book Title';
+      const newAuthor = 'Updated Book Author';
+
+      resolver.updateBook(idToUpdate, newTitle, newAuthor);
+
+      expect(mockUpdateBook).toHaveBeenCalledWith(
+        idToUpdate,
+        newTitle,
+        newAuthor,
+      );
+    });
+  });
+
+  describe('deleteBook', () => {
+    it('should delete an existing book', () => {
+      jest.spyOn(booksService, 'deleteBook').mockReturnValue(true);
+
+      const idToDelete = '1';
+
+      const result = resolver.deleteBook(idToDelete);
+
+      expect(result).toBe(true);
     });
   });
 });
--- api/src/books/books.service.spec.ts
+++ api/src/books/books.service.spec.ts
@@ -15,4 +15,63 @@ describe('BooksService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return an array of books', () => {
+    const result = service.getBooks();
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  describe('createBook', () => {
+    it('should create a new book', () => {
+      const initialLength = service.getBooks().length;
+      const newBook = { id: '6', title: 'New Book', author: 'New Author' };
+
+      service.createBook(newBook);
+
+      const books = service.getBooks();
+      expect(books).toContainEqual(newBook);
+      expect(books.length).toBe(initialLength + 1);
+    });
+  });
+
+  describe('updateBook', () => {
+    it('should update an existing book', () => {
+      const updatedBook = {
+        id: '1',
+        title: 'Updated Title',
+        author: 'Updated Author',
+      };
+
+      service.updateBook(updatedBook.id, updatedBook.title, updatedBook.author);
+
+      const books = service.getBooks();
+      const book = books.find((b) => b.id === updatedBook.id);
+      expect(book).toEqual(updatedBook);
+    });
+
+    it('should return false for non-existing book', () => {
+      const result = service.updateBook(
+        'nonexistent-id',
+        'Updated Title',
+        'Updated Author',
+      );
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('deleteBook', () => {
+    it('should delete an existing book', () => {
+      const bookToDelete = service.getBooks()[0];
+
+      service.deleteBook(bookToDelete.id);
+
+      const books = service.getBooks();
+      expect(books).not.toContainEqual(bookToDelete);
+    });
+
+    it('should return false for non-existing book', () => {
+      const result = service.deleteBook('10');
+      expect(result).toBe(false);
+    });
+  });
 });
