7dac3917e85a50aab74a63d38b23734272825516
Ognjen1994 <ognjen.risti@gmail.com>
2023-09-20 20:21:07 +0200

    UI - add meta tags title and description, update of server base url

--- ui/utils/constants.ts
+++ ui/utils/constants.ts
@@ -1,4 +1,4 @@
 // this would be even better to store in env variables, but due to inclusion of these variables in the gitignore file
 // it wouldn't be visible on git for the sake of demonstration
-export const SERVER_BASE_URL = "http://localhost:3000";
+export const SERVER_BASE_URL = "http://localhost:3000/graphql";
 export const CLIENT_BASE_URL = "http://localhost:3001";
